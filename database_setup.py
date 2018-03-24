64e78d15d7c376bb0489a3fe163b41d5b480c6b8
DanSLuong <hayate58@gmail.com>
2018-03-23 20:48:45 -0500

    On DELETE CASCADE added to database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy import create_engine
 
 Base = declarative_base()
@@ -16,6 +16,9 @@ class User(Base):
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
+    # teams = relationship("Team", backref="user")
+    # players = relationship("Player", backref="user")
+
 
 class Team(Base):
     __tablename__ = 'team'
@@ -26,7 +29,8 @@ class Team(Base):
     state = Column(String(250), nullable=False)
     conference = Column(String(250))
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user = relationship(User, backref='team')
+
 
     @property
     def serialize(self):
@@ -54,10 +58,12 @@ class Player(Base):
     college = Column(String(250))
     birthplace = Column(String(250))
     role = Column(String(250))
+    
     team_id = Column(Integer, ForeignKey('team.id'))
-    team = relationship(Team)
+    team = relationship(Team, backref=backref('player', cascade='all, delete'))
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user = relationship(User, backref='items')
+
 
     @property
     def serialize(self):
